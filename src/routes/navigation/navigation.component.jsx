b374020a9d8a1ce168806cd008638f30fcfc343a
fsaythanry <aldyzkobf@gmail.com>
2022-08-28 00:32:07 +0800

    modified navigation component

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,19 +1,20 @@
 import React from 'react'
 import { Link, Outlet } from 'react-router-dom';
 
+import { ReactComponent as CrownLogo } from '../../assets/crown.svg';
+
+import './navigation.styles.scss';
+
 const Navigation = () => {
     return (
         <>
             <div className='navigation'>
                 <Link className='logo-container' to="/">
-                    <div>Logo</div>
+                    <CrownLogo className='logo' />
                 </Link>
                 <div className="nav-links-container">
-                    <Link className="nav-link" to="/">
-                        Home
-                    </Link>
-                    <Link className="nav-link" to="/shop">
-                        Shop
+                    <Link className='nav-link' to='/shop'>
+                        SHOP
                     </Link>
                 </div>
             </div>
