1e96910c8910a13db39cb00239bedc45862ba041
fsaythanry <aldyzkobf@gmail.com>
2022-09-11 00:18:12 +0800

    created shop component

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router-dom';
 import Navigation from './routes/navigation/navigation.component';
 import Home from './routes/home/home.component';
 import Authentication from './routes/authentication/authentication.component';
+import Shop from './routes/shop/shop.component';
 
 const App = () => {
   return (
@@ -15,10 +16,5 @@ const App = () => {
   );
 };
 
-const Shop = () => {
-  return (
-    <div>shop</div>
-  )
-}
 
 export default App;
