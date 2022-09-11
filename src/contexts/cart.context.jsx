1430db60575230049bbc31c4d7e037580c5a3343
fsaythanry <aldyzkobf@gmail.com>
2022-09-11 21:55:23 +0800

    created addItemToCart in cart context

--- src/contexts/cart.context.jsx
+++ src/contexts/cart.context.jsx
@@ -1,13 +1,36 @@
 import { useState, createContext } from 'react';
 
+const addCartItem = (cartItems, productToAdd) => {
+    // find if cart items contains productToAdd
+    const existingCartItem = cartItems.find((item) => item.id === productToAdd.id);
+    // if found increment quantity
+    if (existingCartItem) {
+        return cartItems.map(item => item.id === productToAdd.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+    }
+
+    // return new array with modified cartItems
+    return [...cartItems, { ...productToAdd, quantity: 1 }];
+}
+
 export const CartContext = createContext({
     isCartOpen: false,
-    setIsCartOpen: () => { }
+    setIsCartOpen: () => { },
+    cartItems: [],
+    addItemToCart: () => { }
 });
 
 export const CartProvider = ({ children }) => {
     const [isCartOpen, setIsCartOpen] = useState(false);
-    const value = { isCartOpen, setIsCartOpen };
+    const [cartItems, setCartItems] = useState([]);
+
+    const addItemToCart = (productToAdd) => {
+        setCartItems(addCartItem(cartItems, productToAdd))
+    }
+
+    const value = { isCartOpen, setIsCartOpen, addItemToCart, cartItems };
 
     return (
         <CartContext.Provider value={value}>{children}</CartContext.Provider>
