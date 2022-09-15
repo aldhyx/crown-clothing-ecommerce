b1df2cff2aebacfc456696c63f40cac5359362ae
fsaythanry <aldyzkobf@gmail.com>
2022-09-16 00:16:50 +0800

    created category detail page and route

--- src/components/category-item/category-item.component.jsx
+++ src/components/category-item/category-item.component.jsx
@@ -4,7 +4,7 @@ const CategoryItem = ({ category }) => {
   const { imageUrl, title } = category;
 
   return (
-    <div className="category-container">
+    <div className="category-item-container">
       <div
         className="background-image"
         style={{
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,5 +1,6 @@
 import { Routes, Route } from 'react-router-dom';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
+import Category from '../category/category.component';
 import './shop.style.scss';
 
 const Shop = () => {
@@ -7,6 +8,7 @@ const Shop = () => {
     return (
         <Routes>
             <Route index element={<CategoriesPreview />} />
+            <Route path=":category" element={<Category />} />
         </Routes>
     )
 }
