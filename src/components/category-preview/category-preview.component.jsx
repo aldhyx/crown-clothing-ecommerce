7ba78a0d45417a9395989079130764c7b3b3d1e1
fsaythanry <aldyzkobf@gmail.com>
2022-09-16 00:27:24 +0800

    modified category detail page

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import ProductCard from '../product-card/product-card.component';
 import './category-preview.style.scss';
 
@@ -5,9 +6,9 @@ const CategoryPreview = ({ title, products }) => {
     return (
         <div className='category-preview-container'>
             <h2>
-                <span className='title'>
+                <Link className='title' to={title}>
                     {title.toUpperCase()}
-                </span>
+                </Link>
             </h2>
 
             <div className='preview'>
