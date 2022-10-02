e9fe105e22ac15bb423cb7a3e469ebad2d3f9987
fsaythanry <aldyzkobf@gmail.com>
2022-10-02 23:16:59 +0800

    created custom logger middleware

--- src/store/store.js
+++ src/store/store.js
@@ -4,7 +4,23 @@ import { rootReducer } from './root-reducer';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-const middleWares = [logger];
+/**
+ * Create custom middleware
+ * As you can see, the middleware is a curried function. 
+ * What is a curried function? A curried function is a function that returns another function.
+*/
+const loggerMiddleware = (store) => (next) => (action) => {
+    if (!action.type) return next(action);
+    console.log('type: ', action.type);
+    console.log('payload: ', action.payload);
+    console.log('currentState: ', store.getState());
+
+    next(action);
+
+    console.log('next state: ', store.getState());
+}
+
+const middleWares = [loggerMiddleware];
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
 const persistConfig = {
