68f36cf55b70584efa1f0130c307742005ecdbd8
fsaythanry <aldyzkobf@gmail.com>
2022-10-02 23:19:25 +0800

    remove custom middleware

--- src/store/store.js
+++ src/store/store.js
@@ -4,23 +4,7 @@ import { rootReducer } from './root-reducer';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 
-/**
- * Create custom middleware
- * As you can see, the middleware is a curried function. 
- * What is a curried function? A curried function is a function that returns another function.
-*/
-const loggerMiddleware = (store) => (next) => (action) => {
-    if (!action.type) return next(action);
-    console.log('type: ', action.type);
-    console.log('payload: ', action.payload);
-    console.log('currentState: ', store.getState());
-
-    next(action);
-
-    console.log('next state: ', store.getState());
-}
-
-const middleWares = [loggerMiddleware];
+const middleWares = [logger];
 const composeEnhancers = compose(applyMiddleware(...middleWares));
 
 const persistConfig = {
